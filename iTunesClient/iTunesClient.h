3f6bb82e57df8fe3638e0f531e6946fd258477b5
mustii <mustii@mmk.one>
2019-06-29 18:34:54 +0300

    Initial Commit

---
+++ iTunesClient/iTunesClient.h
@@ -0,0 +1,19 @@
+//
+//  iTunesClient.h
+//  iTunesClient
+//
+//  Created by Mustafa Khalil on 6/29/19.
+//  Copyright © 2019 mmk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iTunesClient.
+FOUNDATION_EXPORT double iTunesClientVersionNumber;
+
+//! Project version string for iTunesClient.
+FOUNDATION_EXPORT const unsigned char iTunesClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iTunesClient/PublicHeader.h>
+
+
